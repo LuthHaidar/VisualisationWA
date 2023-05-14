77838b6f66acadf64ba703618d4e8b600c9cfe08
StrangeFlame <25YMUHA524I@student.ri.edu.sg>
2023-05-14 21:25:53 +0800

    Implemented new features -canvas saving - regenearting new watercolours

--- sketch.js
+++ sketch.js
@@ -1,26 +1,32 @@
 const DEPTH = 4;
 const BASE_DEFORMATION = 4;
-const TOTAL_LAYERS = 100;
+const TOTAL_LAYERS = 150;
 const OPACITY = 4;
-const RADIUS = 150;
+const RADIUS = 200;
 const SIDES = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   noStroke();
-  noLoop();
+  font = loadFont('assets/RobotoMono-Regular.ttf');
+  textSize(20);
 }
 
 function draw() {
   background(255);
+  let colour = color(random(255), random(255), random(255), OPACITY);
   let points = generateBasePolygon(width/2, height/2, RADIUS, SIDES);
   let basePolygon = generateWatercolor(points, BASE_DEFORMATION);
   for (let i = 0; i < TOTAL_LAYERS; i++) {
     let poly = generateWatercolor(basePolygon, DEPTH);
-    fill(225, 0, 0, OPACITY);
+    fill(colour);
     drawPolygon(poly);
   }
+  fill(0);
+  text('Press any key to generate a new watercolor', width/2 - 200, height / 2 - 450)
+  text('Press s to save the current watercolor', width/2 - 200, height /2 - 400)
+  noLoop();
 }
 
 function drawPolygon(points) {
@@ -29,10 +35,6 @@ function drawPolygon(points) {
     vertex(pt.x, pt.y);
   }
   endShape(CLOSE);
-}
-
-function midpoint(a, b) {
-  return createVector((a.x + b.x) / 2, (a.y + b.y) / 2);
 }
 
 function generateBasePolygon(centerX, centerY, radius, sides) {
@@ -51,20 +53,31 @@ function generateWatercolor(points, depth) {
       return points;
   }
   let newPoints = [];
-  const scaleFactor = 4;
+  const scaleFactor = 5;
   for (let i = 0; i < points.length; i++) {
       let a = points[i];
       let c = points[(i + 1) % points.length];
-      let edgeWeight = random(0.5, 4.0);
-      let maxVariance = dist(a.x, a.y, c.x, c.y) / 2;
+      let edgeWeight = random(0.5, 7);
+      let maxVariance = dist(a.x, a.y, c.x, c.y)/2;
       let variance = randomGaussian(0, depth) * scaleFactor * edgeWeight;
       variance = constrain(variance, -maxVariance, maxVariance);
-      let position = randomGaussian(0.5, 0.15);
-      let b_prime = createVector(
-          lerp(a.x, c.x, position) + variance * cos(atan2(c.y - a.y, c.x - a.x) + HALF_PI),
-          lerp(a.y, c.y, position) + variance * sin(atan2(c.y - a.y, c.x - a.x) + HALF_PI)
-      );
+      let position = random(0, 1);
+      let b_prime = p5.Vector.add(a, p5.Vector.mult(p5.Vector.sub(c, a), position)).add(p5.Vector.mult(p5.Vector.fromAngle(p5.Vector.sub(c, a).heading() + HALF_PI), variance));
       newPoints.push(a, b_prime);
   }
   return generateWatercolor(newPoints, depth - 1);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function keyPressed() { //save canvas without text
+  if (key === 's' || key === 'S') {
+    saveCanvas('watercolor', 'png');
+  }
+}
+
+function mousePressed() {
+  redraw();
 }
